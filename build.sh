1c4c659e66f14e786728f41103801e1e6d107b74
zeromake <a390720046@gmail.com>
2021-09-29 15:50:41 +0800

    flow use bash script build

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ if command -v mcs >/dev/null 2>&1; then
 elif [ -d ${fwRoot} ]; then
     echo "ms building"
     fwdir=`find ${fwRoot} -maxdepth 1 -type d -name 'v*' | sort -r | head -1`
-    "${fwdir}/csc" //nologo //out:shim.exe shim.cs
+    "${fwdir}/csc" //platform:x86 //nologo //out:shim-x86.exe shim.cs
+    "${fwdir}/csc" //platform:x64 //nologo //out:shim-x86-64.exe shim.cs
 fi
 
 file ./*.exe
