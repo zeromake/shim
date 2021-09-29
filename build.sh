0afd07d58c54ff82acd35b4641b65e35cd7b742c
zeromake <a390720046@gmail.com>
2021-09-29 15:54:26 +0800

    fix: log ms build use fw version

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ if command -v mcs >/dev/null 2>&1; then
     mcs -platform:X64 -out:shim-x86-64.exe -optimize shim.cs
     mcs -platform:X86 -out:shim-x86.exe -optimize shim.cs
 elif [ -d ${fwRoot} ]; then
-    echo "ms building"
     fwdir=`find ${fwRoot} -maxdepth 1 -type d -name 'v*' | sort -r | head -1`
+    echo "ms building by ${fwdir}"
     "${fwdir}/csc" //platform:x86 //nologo //out:shim-x86.exe shim.cs
     "${fwdir}/csc" //platform:x64 //nologo //out:shim-x86-64.exe shim.cs
 fi
